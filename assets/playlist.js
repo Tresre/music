6f82741bb452d77c81990d3f24e7fc7d633a9275
Tresre <58750285+Tresre@users.noreply.github.com>
2022-04-29 08:48:40 -0400

    Update and rename index.js to playlist.js

---
+++ assets/playlist.js
@@ -0,0 +1,80 @@
+setPlaylist("pop");
+
+function setPlaylist(p) {
+  if (p === "pop") {
+    var playlist = [
+      "All_About_You$The_Knocks",
+      "All_Alone$Fun",
+      "Bitter_Sweet_Symphony$The_Verve",
+      "Gotta_Get_Up$Harry_Nilsson",
+      "Hallucinations$PVRIS",
+      "Heat_Waves$Glass_Animals",
+      "Levitating$Dua_Lipa",
+      "Live_In_The_Moment$Portugal",
+      "Nice_To_Meet_Ya$Niall_Horan",
+      "No_Rest_For_The_Wicked$Cage_The_Elephant",
+      "Only_Human$Jonas_Brothers",
+      "Parking_Lot_View$Almost_Monday",
+      "Sunflower$Post_Malone",
+      "Talk$Two_Door_Cinema",
+      "Thunder$SHAED"
+    ];
+  }
+  if (p === "country" {
+    var playlist = [
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ];
+  }
+  if (p === "electronic" {
+    var playlist = [
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ];
+  }
+  if (p === "rock" {
+    var playlist = [
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ];
+  }
+}
