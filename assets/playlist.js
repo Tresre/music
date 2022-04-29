8f34d7bcf7be01b7d8f0ef7d03ae9ba2d1b2dbf7
Tresre <58750285+Tresre@users.noreply.github.com>
2022-04-29 11:26:47 -0400

    Update playlist.js

--- assets/playlist.js
+++ assets/playlist.js
@@ -1,34 +1,39 @@
-setPlaylist("pop");
-
 function setPlaylist(playlistChoice) {
   console.log("choice " + playlistChoice);
+  if (playlistChoice == undefined) {
+    playlistChoice("pop");
+    return;
+  }
   if (music !== undefined) {
     var setmusic = music;
     setmusic.pause();
   }
   var playedSongs = [];
   var dupeSongs = [];
-  console.log("pop loaded");
   
-  var playlist = [
-    "All_About_You$The_Knocks",
-    "All_Alone$Fun",
-    "Bitter_Sweet_Symphony$The_Verve",
-    "Gotta_Get_Up$Harry_Nilsson",
-    "Hallucinations$PVRIS",
-    "Heat_Waves$Glass_Animals",
-    "Levitating$Dua_Lipa",
-    "Live_In_The_Moment$Portugal",
-    "Nice_To_Meet_Ya$Niall_Horan",
-    "No_Rest_For_The_Wicked$Cage_The_Elephant",
-    "Only_Human$Jonas_Brothers",
-    "Parking_Lot_View$Almost_Monday",
-    "Sunflower$Post_Malone",
-    "Talk$Two_Door_Cinema",
-    "Thunder$SHAED"
-  ];
+  if (playlistChoice === "pop") {
+    console.log("pop loaded");
+    var playlist = [
+      "All_About_You$The_Knocks",
+      "All_Alone$Fun",
+      "Bitter_Sweet_Symphony$The_Verve",
+      "Gotta_Get_Up$Harry_Nilsson",
+      "Hallucinations$PVRIS",
+      "Heat_Waves$Glass_Animals",
+      "Levitating$Dua_Lipa",
+      "Live_In_The_Moment$Portugal",
+      "Nice_To_Meet_Ya$Niall_Horan",
+      "No_Rest_For_The_Wicked$Cage_The_Elephant",
+      "Only_Human$Jonas_Brothers",
+      "Parking_Lot_View$Almost_Monday",
+      "Sunflower$Post_Malone",
+      "Talk$Two_Door_Cinema",
+      "Thunder$SHAED"
+    ];
+  }
   
   if (playlistChoice === "country") {
+    console.log("country loaded");
     var playlist = [
       "",
       "",
@@ -49,6 +54,7 @@ function setPlaylist(playlistChoice) {
   }
   
   if (playlistChoice === "electronic") {
+    console.log("electronic loaded");
     var playlist = [
       "",
       "",
@@ -69,6 +75,7 @@ function setPlaylist(playlistChoice) {
   }
   
   if (playlistChoice === "rock") {
+    console.log("rock loaded");
     var playlist = [
       "",
       "",
