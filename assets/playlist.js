e54f6777c25f2e56caf7217e59dd7c2bee367f1d
Tresre <58750285+Tresre@users.noreply.github.com>
2022-04-29 10:24:33 -0400

    Update playlist.js

--- assets/playlist.js
+++ assets/playlist.js
@@ -1,4 +1,15 @@
-var playlist = [
+setPlaylist("pop");
+
+function setPlaylist(playlistChoice) {
+  console.log("choice " + playlistChoice);
+  if (music !== undefined) {
+    var setmusic = music;
+    setmusic.pause();
+  }
+  var playedSongs = [];
+  var dupeSongs = [];
+  console.log("pop loaded");
+  var playlist = [
     "All_About_You$The_Knocks",
     "All_Alone$Fun",
     "Bitter_Sweet_Symphony$The_Verve",
@@ -15,36 +26,6 @@ var playlist = [
     "Talk$Two_Door_Cinema",
     "Thunder$SHAED"
   ];
-setPlaylist("pop");
-
-function setPlaylist(playlistChoice) {
-  console.log("choice " + playlistChoice);
-  if (music !== undefined) {
-    var setmusic = music;
-    setmusic.pause();
-  }
-  var playedSongs = [];
-  var dupeSongs = [];
-  if (playlistChoice === "pop") {
-    console.log("pop loaded");
-    var playlist = [
-      "All_About_You$The_Knocks",
-      "All_Alone$Fun",
-      "Bitter_Sweet_Symphony$The_Verve",
-      "Gotta_Get_Up$Harry_Nilsson",
-      "Hallucinations$PVRIS",
-      "Heat_Waves$Glass_Animals",
-      "Levitating$Dua_Lipa",
-      "Live_In_The_Moment$Portugal",
-      "Nice_To_Meet_Ya$Niall_Horan",
-      "No_Rest_For_The_Wicked$Cage_The_Elephant",
-      "Only_Human$Jonas_Brothers",
-      "Parking_Lot_View$Almost_Monday",
-      "Sunflower$Post_Malone",
-      "Talk$Two_Door_Cinema",
-      "Thunder$SHAED"
-    ];
-  }
   if (playlistChoice === "country") {
     var playlist = [
       "",
