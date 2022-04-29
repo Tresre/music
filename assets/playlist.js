b4a7561b83d1bf854a707306c09749709ac677ef
Tresre <58750285+Tresre@users.noreply.github.com>
2022-04-29 10:09:38 -0400

    Update playlist.js

--- assets/playlist.js
+++ assets/playlist.js
@@ -1,20 +1,3 @@
-var playlist = [
-    "All_About_You$The_Knocks",
-    "All_Alone$Fun",
-    "Bitter_Sweet_Symphony$The_Verve",
-    "Gotta_Get_Up$Harry_Nilsson",
-    "Hallucinations$PVRIS",
-    "Heat_Waves$Glass_Animals",
-    "Levitating$Dua_Lipa",
-    "Live_In_The_Moment$Portugal",
-    "Nice_To_Meet_Ya$Niall_Horan",
-    "No_Rest_For_The_Wicked$Cage_The_Elephant",
-    "Only_Human$Jonas_Brothers",
-    "Parking_Lot_View$Almost_Monday",
-    "Sunflower$Post_Malone",
-    "Talk$Two_Door_Cinema",
-    "Thunder$SHAED"
-  ];
 setPlaylist("pop");
 
 function setPlaylist(playlistChoice) {
