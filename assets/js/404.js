8dbf2ed9f3b4e03d0acc6b561cc82a1a708ef1a8
Tresre <58750285+Tresre@users.noreply.github.com>
2022-08-15 00:00:55 -0400

    Update 404.js

--- assets/js/404.js
+++ assets/js/404.js
@@ -5,8 +5,18 @@ function redirect() {
   var playlist = location.pathname.split("/")[
     PATH_SEGMENTS_TO_SKIP + 1
   ];
-  console.log("https://tresre.dev/music/?" + playlist);
-  window.location.href = "https://tresre.dev/music/?" + playlist;
+  if (playlist == "song") {
+    var song = location.pathname.split("/")[
+      PATH_SEGMENTS_TO_SKIP + 2
+    ];
+    console.log("https://tresre.dev/music/?" + playlist + "/" + song);
+    window.location.href = "https://tresre.dev/music/?" + playlist;
+    mots(song);
+    console.log(song);
+  } else {
+    console.log("https://tresre.dev/music/?" + playlist);
+    window.location.href = "https://tresre.dev/music/?" + playlist;
+  }
 }
 
 redirect();
